115d7df5d560de13daefcffc9691f6593bad57bc
Ajul Thomas <ajul.thomas@outlook.com>
2025-02-20 23:42:56 +1100

    redesigned the gtfs-builder layout

---
+++ src/app/agency-details/agency-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { MatCard, MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'agency-details',
+  imports: [MatCardModule],
+  templateUrl: './agency-details.component.html',
+  styleUrl: './agency-details.component.scss',
+})
+export class AgencyDetailsComponent {}
