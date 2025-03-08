b395d382d29874b955bb61d10c3afefb77de9379
Ajul Thomas <ajul.thomas@outlook.com>
2025-03-08 13:39:14 +1100

    SCRUM-67 code refactoring

--- src/app/services/excel-utils.service.ts
+++ src/app/services/excel-utils.service.ts
@@ -1,6 +1,12 @@
 import { Injectable, signal, Signal, WritableSignal } from '@angular/core';
 import { read, utils, WorkSheet } from 'xlsx';
 import { MessageService } from './message.service';
+import {
+  DepotDetails,
+  RouteData,
+  RouteSchedule,
+  ScheduleData,
+} from '../types/route-data.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -165,36 +171,4 @@ export class ExcelUtilsService {
   }
 
   // end of class
-}
-
-// create interface for the route schedule
-export interface RouteSchedule {
-  round: string;
-  num: number;
-  direction: string;
-  busStop: string;
-  latitude: number | string;
-  longitude: number | string;
-  coordinate: string | number;
-  arrival: string | Date;
-  departure: string | Date;
-}
-
-export interface DepotDetails {
-  depo: string;
-  latitude: number | string;
-  longitude: number | string;
-  coordinate: string | number;
-  arrival: string | Date;
-  departure: string | Date;
-}
-
-export interface ScheduleData {
-  depotDepartureDetails: DepotDetails;
-  depotArrivalDetails: DepotDetails;
-  routeSchedule: RouteSchedule[];
-}
-
-export interface RouteData {
-  [scheduleName: string]: ScheduleData;
 }
---
+++ src/app/types/gtfs.interface.ts
@@ -0,0 +1,56 @@
+/**
+ * @reference https://gtfs.org/documentation/schedule/reference/#routestxt
+ */
+export interface ServiceRouteDetails {
+  route_id: string;
+  agency_id: string;
+  route_short_name: string;
+  route_long_name: string;
+  route_desc?: string;
+  route_type: number; // will always be 3 for bus routes
+  route_url?: string;
+  route_color?: string;
+  route_text_color?: string;
+  route_sort_order?: string;
+}
+
+/**
+ * @reference https://gtfs.org/documentation/schedule/reference/#stopstxt
+ */
+export interface StopDetails {
+  stop_id: string;
+  stop_code: string;
+  stop_name: string;
+  tts_stop_name?: string;
+  stop_desc?: string;
+  stop_lat: number;
+  stop_lon: number;
+  zone_id?: string;
+  stop_url?: string;
+  location_type?: number;
+  parent_station?: string;
+  stop_timezone?: string;
+  wheelchair_boarding?: number; // always 0 for bus stops
+  level_id?: string;
+  platform_code?: string;
+}
+
+/**
+ * @reference https://gtfs.org/documentation/schedule/reference/#stop_timestxt
+ */
+
+/**
+ * @reference https://gtfs.org/documentation/schedule/reference/#tripstxt
+ */
+export interface TripDetails {
+  route_id: string;
+  service_id: string;
+  trip_id: string;
+  trip_headsign?: string;
+  trip_short_name?: string;
+  direction_id?: number;
+  block_id?: string;
+  shape_id?: string;
+  wheelchair_accessible?: number;
+  bikes_allowed?: number;
+}
