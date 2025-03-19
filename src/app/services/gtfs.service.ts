e4e2c06a2d832a404562839b771ae6814d8ab14e
Ajul Thomas <ajul.thomas@outlook.com>
2025-03-19 18:11:28 +1100

    downloads routes and stops

--- src/app/services/gtfs.service.ts
+++ src/app/services/gtfs.service.ts
@@ -10,36 +10,97 @@ import {
 } from '../types/gtfs.interface';
 import JSZip from 'jszip';
 import { saveAs } from 'file-saver';
+import { ExcelUtilsService } from './excel-utils.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GtfsService {
-  constructor() {}
+  constructor(private excelUtilsService: ExcelUtilsService) {}
 
   gtfsData: GTFSData = {
     agency: AGENCY_DATA,
     calendar: CALENDAR_DATA,
+    routes: [],
+    stops: [],
   };
 
   gtfsFiles: GTFSFiles = {
     agency: '',
     calendar: '',
+    routes: '',
+    stops: '',
   };
 
-  serviceRoutes: RouteDetails[] = [];
+  // serviceRoutes: RouteDetails[] = [];
   stops: StopDetails[] = [];
 
-  processGTFSInfo(routeData: RouteData) {
-    this.extractRoutes(routeData);
-    this.extractStops(routeData);
+  extractGTFSInfo() {
+    this.extractRoutes();
+    this.extractStops();
   }
 
-  extractRoutes(routeData: RouteData) {}
+  extractRoutes() {
+    const routes = this.excelUtilsService.routes;
+    let i = 0;
+    for (const route of routes) {
+      i += 1;
+      const routeID: string = `BST${i.toString().padStart(2, '0')}`;
+      const routeShortName: string = this.getRouteShortName(route);
+      const routeDetails: RouteDetails = {
+        route_id: routeID,
+        agency_id: 'BST',
+        route_short_name: routeShortName,
+        route_long_name: route,
+        route_type: 3,
+        route_color: ROUTE_COLOURS[i - 1],
+        route_text_color: '#FFFFFF',
+      };
+      this.gtfsData.routes.push(routeDetails);
+    }
+  }
 
-  extractStops(routeData: RouteData) {}
+  getRouteShortName(route: string): string {
+    const [start, end] = route.split('-');
+
+    // the first character of the space separated words
+    const startShort = start
+      .split(' ')
+      .map((word) => word[0])
+      .join('');
+    const endShort = end
+      .split(' ')
+      .map((word) => word[0])
+      .join('');
+
+    return `${startShort}-${endShort}`;
+  }
+
+  extractStops() {
+    const stops = this.excelUtilsService.stops;
+    let i = 0;
+    for (const [key, value] of stops) {
+      i = i + 1;
+      const stopDetails: StopDetails = {
+        stop_id: `HLT${i.toString().padStart(2, '0')}`,
+        stop_code: key,
+        stop_name: value.name,
+        stop_desc: '',
+        stop_lat: value.latitude,
+        stop_lon: value.longitude,
+        zone_id: 'SOLO',
+        stop_url: '',
+        location_type: 0,
+        parent_station: '',
+        stop_timezone: 'Asia/Jakarta',
+        wheelchair_boarding: 0,
+      };
+      this.gtfsData.stops.push(stopDetails);
+    }
+  }
 
   downloadGTFS() {
+    this.extractGTFSInfo();
     const zip = new JSZip();
     // covert the objects to GTFS files
     for (const [filename, data] of Object.entries(this.gtfsData)) {
@@ -55,11 +116,13 @@ export class GtfsService {
 
   createFiles(data: any[]): string {
     let fileContent = '';
-    const keys = Object.keys(data[0]);
-    fileContent += keys.join(',') + '\n';
-    for (const item of data) {
-      const values = Object.values(item);
-      fileContent += values.join(',') + '\n';
+    if (data.length > 0) {
+      const keys = Object.keys(data[0]);
+      fileContent += keys.join(',') + '\n';
+      for (const item of data) {
+        const values = Object.values(item);
+        fileContent += values.join(',') + '\n';
+      }
     }
     return fileContent;
   }
@@ -72,9 +135,9 @@ export const AGENCY_DATA: AgencyDetails[] = [
     agency_url: 'https://pariwisatasolo.surakarta.go.id',
     agency_timezone: 'Asia/Jakarta',
     agency_lang: 'en',
-    // agency_phone: '',
-    // agency_fare_url: 'https://pariwisatasolo.surakarta.go.id/',
-    // agency_email: '',
+    agency_phone: '',
+    agency_fare_url: 'https://pariwisatasolo.surakarta.go.id/',
+    agency_email: '',
   },
 ];
 
@@ -91,4 +154,13 @@ export const CALENDAR_DATA: CalendarDetails[] = [
     start_date: '20250101',
     end_date: '20301231',
   },
+];
+
+export const ROUTE_COLOURS: string[] = [
+  '#FF0000',
+  '#FFA500',
+  '#FFFF00',
+  '#008000',
+  '#0000FF',
+  '#4B0082',
 ];
