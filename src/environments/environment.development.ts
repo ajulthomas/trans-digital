4256f71a624a18db76872cf2a94c18270562bf16
Ajul Thomas <ajul.thomas@outlook.com>
2025-03-19 22:15:11 +1100

    updates the env vars

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  version: '0.3.0',
-  buildDate: '2025-03-10',
+  version: '0.4.0',
+  buildDate: '19-02-2025 10:14:22 PM AEDT',
   buildEnv: 'development',
 };
