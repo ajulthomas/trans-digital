9be97266b806a0ffce734fc7dc41e1cf120f45a9
Ajul Thomas <ajul.thomas@outlook.com>
2025-04-01 16:46:44 +1100

    configures routes and enables routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { GtfsBuilderComponent } from './gtfs-builder/gtfs-builder.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/gtfs-builder', pathMatch: 'full' },
+  { path: 'gtfs-builder', component: GtfsBuilderComponent },
+];
