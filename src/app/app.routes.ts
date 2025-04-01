ac552b34b4417ded61e6404f44fdebac41c4c6b8
Ajul Thomas <ajul.thomas@outlook.com>
2025-04-01 20:49:11 +1100

    adds map

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { GtfsBuilderComponent } from './gtfs-builder/gtfs-builder.component';
+import { RouteMapComponent } from './route-map/route-map.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/gtfs-builder', pathMatch: 'full' },
   { path: 'gtfs-builder', component: GtfsBuilderComponent },
+  { path: 'route-map', component: RouteMapComponent },
+  { path: '**', redirectTo: '/gtfs-builder' },
 ];
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  version: '0.6.0',
-  buildDate: '01-04-2025 14:34:07 AEDT',
+  version: '0.7.0',
+  buildDate: '01-04-2025 19:45:07 AEDT',
   buildEnv: 'production',
 };
