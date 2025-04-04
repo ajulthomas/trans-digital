4fd4e76865d2586fafe2864634b73acc9ef7fd52
Ajul Thomas <ajul.thomas@outlook.com>
2025-04-05 02:29:20 +1100

    removes keys from env files

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,5 +3,4 @@ export const environment = {
   version: '0.8.0',
   buildDate: '01-04-2025 19:45:07 AEDT',
   buildEnv: 'development',
-  googleMapsApiKey: 'AIzaSyC8lV7laCcoyCg5MPrJVULN-N_7A5ATMgE',
 };
